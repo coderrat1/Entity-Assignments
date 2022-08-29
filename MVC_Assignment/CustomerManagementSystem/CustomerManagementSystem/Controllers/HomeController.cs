338a3d225aac8f32e5513b7b9aa43cc4b612daf4
coderrat1 <vermashubhamfy@gmail.com>
2022-08-29 14:15:34 +0530

    committing

---
+++ MVC_Assignment/CustomerManagementSystem/CustomerManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,255 @@
+﻿using CustomerManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CustomerManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Create Customer
+        public ActionResult Index()
+        {
+            return View("CreateCustomer");
+        }
+
+        [HttpPost]
+        public ActionResult Index(CUSTOMER customer)
+        {
+            int i = new Repository().CreateCustomer(customer);
+
+            ViewBag.SuccessMessage = "Customer Created Successfully...";
+            ViewBag.DangerMessage = string.Empty;
+
+            if (i == -1)
+            {
+                ViewBag.SuccessMessage = string.Empty;
+                ViewBag.DangerMessage = "Customer Created UnSuccessfull...";
+                
+            }
+            else
+            {
+                ViewBag.ID = "Generated Customer ID is: " + i; 
+            }
+            
+                
+            return View("CreateCustomer");
+        }
+        #endregion
+
+        #region Update Customer
+        public ActionResult Edit(int id)
+        {
+            TempData["res"] = id.ToString();
+            return View("UpdateCustomer", new Repository().SearchCustomer(id));
+        }
+
+        public ActionResult Update()
+        {            
+            return View("SearchForUpdate");
+        }
+
+
+        [HttpPost]
+        public ActionResult SearchForUpdate()
+        {
+            CUSTOMER cust = null;
+            ViewBag.NoData = string.Empty;
+
+            if (Request["input"] != null)
+            {
+                TempData["res"] = Request["input"].ToString();
+
+                if (int.TryParse(Request["input"].ToString(), out int id))
+                {
+                    cust = new Repository().GetCustomer(id);
+                }
+                else
+                {
+                    cust = new Repository().GetCustomer(Request["input"].ToString());
+                }
+            }
+
+            
+            if (cust == null)
+            {
+                ViewBag.NoData = "No Data Found...";
+                return View("SearchForUpdate");
+            }
+            return View("UpdateCustomer", cust);
+        }
+
+
+        [HttpPost]
+        public ActionResult Update(CUSTOMER customer)
+        {
+            int t = -10;
+            if (TempData["res"] != null)
+            {
+                if (int.TryParse(TempData["res"].ToString(), out int id))
+                {
+                    t = new Repository().ModifyCustomer(customer, id);
+                }
+                else
+                {
+                    t = new Repository().ModifyCustomer(customer, TempData["res"].ToString());
+                }
+            }
+
+            if (t == 1)
+            {
+                ViewBag.SuccessMessage = "Customer Updated Successfully...";
+                ViewBag.DangerMessage = string.Empty;
+            }
+            else if (t == -1)
+            {
+                ViewBag.SuccessMessage = string.Empty;
+                ViewBag.DangerMessage = "Customer Updated UnSuccessfull...";
+            }
+            else
+            {
+                ViewBag.SuccessMessage = string.Empty;
+                ViewBag.DangerMessage = "Something Went Wrong...";
+            }
+
+            return View("SearchForUpdate");
+        }
+        #endregion
+
+
+
+        #region Delete Customer
+        public ActionResult DeleteFromAllDetails(int id)
+        {
+            TempData["resDel"] = id.ToString();
+            return View("DeleteCustomer", new Repository().SearchCustomer(id));
+        }
+
+        public ActionResult Delete()
+        {
+            return View("SearchForDelete");
+        }
+
+        [HttpPost]
+        public ActionResult SearchForDelete()
+        {
+            CUSTOMER cust = null;
+            ViewBag.NoData = string.Empty;
+
+            if (Request["input"].ToString() != null)
+            {
+                TempData["resDel"] = Request["input"].ToString();
+
+                if (int.TryParse(Request["input"].ToString(), out int id))
+                {
+                    cust = new Repository().GetCustomer(id);
+                }
+                else
+                {
+                    cust = new Repository().GetCustomer(Request["input"].ToString());
+                }
+            }
+
+            
+            if (cust == null)
+            {
+                ViewBag.NoData = "No Data Found...";
+                return View("SearchForDelete");
+            }
+            return View("DeleteCustomer", cust);
+        }
+
+
+        [HttpPost]
+        public ActionResult Remove()
+        {
+            try
+            {
+                int t = -10;
+                if (TempData["resDel"] != null)
+                {
+                    if (int.TryParse(TempData["resDel"].ToString(), out int id))
+                    {
+                        t = new Repository().RemoveCustomer(id);
+                    }
+                    else
+                    {
+                        t = new Repository().RemoveCustomer(TempData["resDel"].ToString());
+                    }
+                }
+
+                if (t == 1)
+                {
+                    ViewBag.SuccessMessage = "Customer Removed Successfully...";
+                    ViewBag.DangerMessage = string.Empty;
+                }
+                else if (t == -1)
+                {
+                    ViewBag.SuccessMessage = string.Empty;
+                    ViewBag.DangerMessage = "Customer Removed UnSuccessfully...";
+                }
+                else
+                {
+                    ViewBag.SuccessMessage = string.Empty;
+                    ViewBag.DangerMessage = "Something Went Wrong...";
+                }
+            }
+            catch{ }
+
+            return View("SearchForDelete");
+        }
+        #endregion
+
+
+        #region Search Customer
+
+        public ActionResult Search()
+        {
+            return View("SearchCustomer");
+        }
+
+        [HttpPost]
+        public ActionResult Search(CUSTOMER customer)
+        {
+            CUSTOMER cust = null;
+            ViewBag.NoData = string.Empty;
+
+            if (Request["input"] != null)
+            {
+                TempData["res"] = Request["input"].ToString();
+
+                if (int.TryParse(Request["input"].ToString(), out int id))
+                {
+                    cust = new Repository().GetCustomer(id);
+                }
+                else
+                {
+                    cust = new Repository().GetCustomer(TempData["res"].ToString());
+                }
+            }
+
+            
+            if (cust == null)
+            {
+                ViewBag.NoData = "No Data Found...";
+                return View("SearchCustomer");
+            }
+
+            return View("Details", cust);
+        }
+        #endregion
+
+
+        #region All Details
+        public ActionResult Display()
+        {
+
+            return View("AllDetails", new Repository().GetAllCustomers());
+        }
+        #endregion
+
+    }
+}
---
+++ MVC_Assignment/CustomerManagementSystem/CustomerManagementSystem/Models/Repository.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagementSystem.Models
+{
+    public class Repository
+    {
+        CMS_DBContext _dbContext;
+
+        public Repository()
+        {
+            _dbContext = new CMS_DBContext();
+        }
+
+        #region Get All Customers
+        public List<CUSTOMER> GetAllCustomers()
+        {
+            return _dbContext.CUSTOMERs.ToList();
+        }
+        #endregion
+
+        #region Get Customer
+        public CUSTOMER GetCustomer(int id)
+        {
+           return _dbContext.CUSTOMERs.FirstOrDefault(x => x.CustomerId == id);
+        }
+        
+        public CUSTOMER GetCustomer(string name)
+        {
+            return _dbContext.CUSTOMERs.FirstOrDefault(x => x.CustomerName.StartsWith(name) && name.Trim().Length > 0);
+        }
+        #endregion
+
+        #region Create Customer
+        public int CreateCustomer(CUSTOMER customer)
+        {
+            try
+            {
+                CUSTOMER cust = new CUSTOMER
+                {
+                    CustomerName = customer.CustomerName.Trim(),
+                    City = customer.City.Trim(),
+                    Age = customer.Age,
+                    Phone = customer.Phone,
+                    PinCode = customer.PinCode
+                };
+
+                _dbContext.CUSTOMERs.Add(cust);
+                _dbContext.SaveChanges();
+
+                return cust.CustomerId;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        #endregion
+
+        #region Modify Customer
+        public int ModifyCustomer(CUSTOMER customer, int id)
+        {
+            try
+            {
+                CUSTOMER c = _dbContext.CUSTOMERs.FirstOrDefault(x => x.CustomerId == id);
+                if (c != null)
+                {
+                    c.CustomerName = customer.CustomerName;
+                    c.City = customer.City;
+                    c.Age = customer.Age;
+                    c.Phone = customer.Phone;
+                    c.PinCode = customer.PinCode;
+
+                    _dbContext.SaveChanges();
+                    return 1;
+                }
+                else
+                    return -1;
+            }
+            catch
+            {
+                return -1;
+            }
+            
+        }
+
+        public int ModifyCustomer(CUSTOMER customer, string name)
+        {
+            try
+            {
+                CUSTOMER c = _dbContext.CUSTOMERs.FirstOrDefault(x => x.CustomerName.StartsWith(name) && name.Trim().Length > 0);
+                if (c != null)
+                {
+                    c.CustomerName = customer.CustomerName;
+                    c.City = customer.City;
+                    c.Age = customer.Age;
+                    c.Phone = customer.Phone;
+                    c.PinCode = customer.PinCode;
+
+                    _dbContext.SaveChanges();
+                    return 1;
+                }
+                else
+                    return -1;
+            }
+            catch
+            {
+                return -1;
+            }
+
+        }
+        #endregion
+
+        #region Search Customer
+        public CUSTOMER SearchCustomer(int id)
+        {
+            return _dbContext.CUSTOMERs.Find(id);
+        }
+
+        public CUSTOMER SearchCustomer(string name)
+        {
+            return _dbContext.CUSTOMERs.Find(name);
+        }
+        #endregion
+
+        #region Remove Customer
+        public int RemoveCustomer(int id)
+        {
+            try
+            {
+                CUSTOMER c = _dbContext.CUSTOMERs.Find(id);
+                if (c != null)
+                {
+                    _dbContext.CUSTOMERs.Remove(c);
+                    _dbContext.SaveChanges();
+                    return 1;
+                }
+                else { return -1; }
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        
+        public int RemoveCustomer(string name)
+        {
+            try
+            {
+                CUSTOMER c = _dbContext.CUSTOMERs.FirstOrDefault(x => x.CustomerName.StartsWith(name) && name.Trim().Length > 0);
+                if (c != null)
+                {
+                    _dbContext.CUSTOMERs.Remove(c);
+                    _dbContext.SaveChanges();
+                    return 1;
+                }
+                else { return -1; }
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        #endregion
+
+    }
+}
