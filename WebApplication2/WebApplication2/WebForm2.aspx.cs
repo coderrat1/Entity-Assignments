3fba8f0d0ad7b569bff27f85d7549794f4ee3106
coderrat1 <vermashubhamfy@gmail.com>
2022-08-29 13:41:38 +0530

    committing

---
+++ WebApplication2/WebApplication2/WebForm2.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String cs = "data source=.; database=master;integrated security=sspi";
+            SqlConnection con = new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand("select * from Customer1", con);
+            try
+            {
+                if (con.State == System.Data.ConnectionState.Closed) 
+                {
+                    con.Open();
+                }
+        
+                SqlDataReader dr = cmd.ExecuteReader();
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+
+             catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+                
+
+
+
+
+        }
+
+        protected void Unnamed1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ connected/connected/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connected
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlcon = "Data Source=DESKTOP-S8BF64H\SQLEXPRESS; Initial Catalog=master; Integrated security=SSPI";
+
+           using(SqlConnection con =new SqlConnection(sqlcon))
+            {
+                string query = "select * from customer1";
+                SqlCommand cmdobj = new SqlCommand();
+                con.open();
+                cmdobj.ExecuteReader();
+                
+            }
+        }
+    }
+}
